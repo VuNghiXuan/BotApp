76c8b17f228ae614c2fe7cd51f63b3b068d486bd
vunghixuan <vunghixuan.info.gmail.com>
2025-04-28 06:05:28 +0700

    format sheets

--- vunghixuan/bot_station/to_excel.py
+++ vunghixuan/bot_station/to_excel.py
@@ -29,6 +29,37 @@ class ExcelWithTOC:
                 self.wb.close()
         if self.app:
             self.app.quit()
+    
+    def format_worksheet(self, ws):
+        # Tự động điều chỉnh kích thước cột và hàng
+        ws.autofit('columns')
+        ws.autofit('rows')
+        
+        # DÒng hyperlink
+        hyper_row = ws.used_range.rows[0]
+        hyper_row.font.color = (255, 0, 0)  # Màu chứ đỏ
+
+        # Lấy dòng đầu tiên
+        first_row = ws.used_range.rows[1]
+        first_row.api.WrapText = True
+
+        # # Bọc văn bản cho các ô có độ dài lớn hơn 30 ký tự
+        # for col in ws.used_range.columns:
+        #     for cell in col:
+        #         if len(str(cell.value)) > 30:
+        #             cell.api.WrapText = True
+
+        # Định dạng dòng đầu tiên
+        first_row.color = (0, 139, 0)  # Màu nền xanh lá cây đậm
+        first_row.font.color = (255, 255, 255)  # Màu chữ trắng
+        first_row.font.bold = True  # In đậm
+        # Canh giữa theo chiều ngang
+        first_row.api.HorizontalAlignment = xw.constants.HAlign.xlHAlignCenter
+
+        # Canh giữa theo chiều dọc
+        first_row.api.VerticalAlignment = xw.constants.VAlign.xlVAlignCenter
+
+        
 
     def add_sheet_with_data(self):
         """Thêm các sheet mới với dữ liệu từ DataFrame và hyperlink quay về Mục Lục."""
@@ -57,10 +88,11 @@ class ExcelWithTOC:
                     ws.range('A2').options(index=False).value = df
                 else:
                     ws.range('A2').value = f'Không tìm thấy dữ liệu cho: "{sheet_name}"'
+                
+                # Định dạng sheets
+                self.format_worksheet(ws)
 
-                ws.autofit('columns')
-                ws.autofit('rows')
-
+                ws.used_range.rows[0]
             except Exception as e:
                 print(f"Lỗi khi thêm sheet '{sheet_name}': {e}")
 
@@ -69,15 +101,16 @@ class ExcelWithTOC:
     def _update_toc(self):
         """Cập nhật sheet Mục Lục với danh sách các sheet và hyperlink."""
         self.toc_sheet.clear_contents()
-        self.toc_sheet.range('A1').value = 'Danh Mục Sheets'
+        self.toc_sheet.range('B1').value = 'Danh Mục Sheets'
         for i, sheet in enumerate(self.wb.sheets):
             if sheet.name != self.toc_sheet_name:
                 # Điều chỉnh chỉ số hàng để bỏ qua hàng tiêu đề 'Danh Mục Sheets'
                 row_index_in_toc = i + 1
-                cell = self.toc_sheet.range(f'A{row_index_in_toc + 1}') # Bắt đầu từ hàng 2
+                self.toc_sheet.range(f'A{row_index_in_toc + 1}').value =  row_index_in_toc-1# Bắt đầu từ hàng 2
+                cell = self.toc_sheet.range(f'B{row_index_in_toc + 1}') # Bắt đầu từ hàng 2
 
                 # Thêm số thứ tự vào giá trị của cell
-                cell.value = f'{row_index_in_toc}. {sheet.name}'
+                cell.value = sheet.name
 
                 sub_address_forward = f"'{sheet.name}'!A1"
                 print(f"Đang tạo hyperlink đến: #{sub_address_forward} trên sheet '{self.toc_sheet_name}'")
